6022c796f364c989ac10cb96a2a3c95b0113be9f
Juand990 <jdng990@gmail.com>
2023-07-10 18:11:07 +0200

    subir proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { HomeComponent } from './home/home/home.component';
+import { CharactersComponent } from './home/characters/characters.component';
+import { DetalleComponent } from './home/characters/detalle/detalle.component';
+import { AboutComponent } from './home/about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    HomeComponent,
+    CharactersComponent,
+    DetalleComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/home/characters/detalle/detalle.component.ts
@@ -0,0 +1,14 @@
+import { Component,Input } from '@angular/core';
+
+@Component({
+  selector: 'app-detalle',
+  templateUrl: './detalle.component.html',
+  styleUrls: ['./detalle.component.css']
+})
+export class DetalleComponent {
+
+  @Input()data: any = [];
+
+
+
+}
